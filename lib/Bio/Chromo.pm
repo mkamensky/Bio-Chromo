2301ee664c31772f5ee1aba396278844832124a9
Moshe Kamensky <kamensky@cpan.org>
2013-09-17 15:45:57 +0300

    initial commit

---
+++ lib/Bio/Chromo.pm
@@ -0,0 +1,19 @@
+# Created: Tue Sep 17 15:45:57 2013
+# Last Changed: TIMESTAMP
+
+package Bio::Chromo;
+use 5.10.0;
+use warnings;
+use integer;
+use strict;
+use Carp;
+use autodie qw(:all);
+
+1;
+
+__END__
+
+=head1 BUGS
+
+=cut
+
